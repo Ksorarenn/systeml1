adb2bcc4706cd17ff02ca7afa6ea9598b1ee9fb2
Ksorarenn <Ksor@renn>
2025-09-06 11:09:31 +0300

    Uncluding unit tests

---
+++ src/MainTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void rec() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void cycle() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void div() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void arrs() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void bigint() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void bits() {
+    }
+}
